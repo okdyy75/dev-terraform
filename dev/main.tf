7aecc59af41f146d4e3f61af198bba7c96d65d3b
okdyy75 <okdyy75@gmail.com>
2022-03-01 12:59:43 +0900

    appRunner用terraform作成

--- dev/main.tf
+++ dev/main.tf
@@ -33,3 +33,72 @@ variable "y_oka_domain" {
   type        = string
 }
 
+############################################################
+### IAM 
+############################################################
+resource "aws_iam_role" "y-oka-apprunner-role" {
+  name = "dev-y-oka-apprunner-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+  ]
+}
+
+############################################################
+### AppRunner
+############################################################
+resource "time_sleep" "wait-create-y-oka-apprunner-role" {
+  create_duration = "5s"
+  depends_on = [
+    aws_iam_role.y-oka-apprunner-role
+  ]
+}
+
+resource "aws_apprunner_service" "y-oka-apprunner" {
+  service_name = "dev-y-oka-apprunner"
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.y-oka-apprunner-role.arn
+    }
+    image_repository {
+      image_configuration {
+        port = "80"
+      }
+      image_identifier      = "162699281520.dkr.ecr.ap-northeast-1.amazonaws.com/y-oka/nginx:latest"
+      image_repository_type = "ECR"
+    }
+  }
+  depends_on = [
+    time_sleep.wait-create-y-oka-apprunner-role
+  ]
+}
+
+############################################################
+### Route 53 
+############################################################
+resource "aws_apprunner_custom_domain_association" "y-oka-apprunner-custom-domain" {
+  service_arn          = aws_apprunner_service.y-oka-apprunner.arn
+  domain_name          = var.y_oka_domain
+  enable_www_subdomain = true
+}
+
+# DNSプロバイダー（自分の場合はgoogleドメイン）に証明書の検証を登録する
+output "y-oka-apprunner-custom-domain-certificate-validation-records" {
+  value = aws_apprunner_custom_domain_association.y-oka-apprunner-custom-domain.certificate_validation_records
+}
+
+# DNSプロバイダー（自分の場合はgoogleドメイン）にCNAMEを登録する
+output "y-oka-apprunner-custom-domain-dns-target" {
+  value = "${var.y_oka_domain} = ${aws_apprunner_custom_domain_association.y-oka-apprunner-custom-domain.dns_target}"
+}
